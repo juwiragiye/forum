3e499aee94edcdc5179ad440ed772b5126ecc705
juwiragiye <uwiragiyejanvi@gmail.com>
2021-11-27 23:28:08 -0500

    :tada: Initial Commit!

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <app-nav />
+    </div>
+    <router-view class="container" />
+  </div>
+</template>
+<script>
+import AppNav from './components/AppNav.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppNav,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="jumbotron border-secondary">
+      <h1 class="display-4">Welcome to community.</h1>
+      <p class="lead">
+        A fully open source project from coding garden with CJ, made with 💚.
+      </p>
+      <hr class="my-4" />
+      <p>This forum is all about community and love</p>
+      <p class="lead">
+        <a class="btn btn-dark btn-lg" href="#" role="button">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
